61826e800256e4a5c1cf42ddd5bdea053d6c167e
Alex <alex@brainiumlabs.com.au>
2024-10-09 09:37:05 +0930

    directory inner, social section

--- src/site/includes/content/_directory-inner.php
+++ src/site/includes/content/_directory-inner.php
@@ -1,95 +1,104 @@
 <div class="page-content page-content--directory-inner">
-	<div class="page-content__wrap">
-		<div class="top-content">
-			<div clas="top-content-title">
-				<Strong>About</Strong>
-			</div>
-			<div class="top-content-logo-hold">
-				<img src="assets/images/business-logo-1.png" alt="Placeholder" />
-			</div>
-			<div class="top-content-text-wrap">
-				<p>
-					We provide comprehensive business IT and Marketing solutions. Our strategy is backed by a proven record of
-					enhancing
-					the return on investment for your business.We provide comprehensive business IT and Marketing solutions. Our
-					strategy is backed by a proven record of enhancing the return on investment for your business. We provide
-					comprehensive business IT and Marketing solutions. Our strategy is backed by a proven record of enhancing the
-					return
-					on investment for your business. We provide comprehensive business IT and Marketing solutions.</p>
-				<p>
-					Our strategy is backed by a proven record of enhancing the return on investment for your business. We provide
-					comprehensive business IT and Marketing solutions. Our strategy is backed by a proven record of enhancing the
-					return
-					on investment for your business. We provide comprehensive business IT and Marketing solutions. Our strategy is
-					backed by a proven record of enhancing the return on investment for your business. We provide comprehensive
-					business
-					IT and Marketing solutions. Our strategy is backed by a proven record of enhancing the return on investment
-					for
-					your
-					business.
-				</p>
-				<p><a href="#" class="read-more link-bold"> Read more</a></p>
-				<div class="meta-data mb-5">
-					<div class="meta-data-row meta-data-row--address mb-2">
-						<div class="meta-data__label"><strong>Address</strong></div>
-						<div class="meta-data__value">Unit 505 / 66 Smith Street, Darwin City, Northern Territory, Australia, 0800
-						</div>
-					</div>
-					<div class="meta-data-row meta-data-row--phone">
-						<div class="meta-data__label"><strong>Phone</strong></div>
-						<div class="meta-data__value"><a href="tel:0470695869">0470695869</a>
-						</div>
-					</div>
-					<div class="meta-data-row meta-data-row--email">
-						<div class="meta-data__label"><strong>Email</strong></div>
-						<div class="meta-data__value"><a href="nailto:info@21webs.com.au">info@21webs.com.au</a>
-						</div>
-					</div>
-					<div class="meta-data-row meta-data-row--web">
-						<div class="meta-data__label"><strong>Website</strong></div>
-						<div class="meta-data__value"><a href="https://21webs.com.au">21webs.com.au</a>
-						</div>
-					</div>
-				</div>
-				<div class="card-filter-area mb-5">
-					<div class="title"><strong>Service area</strong></div>
-					<ul class="card-badges list-unstyled list-inline">
-						<li><span class="badge text-bg-light">Sales</span></li>
-						<li><span class="badge text-bg-light">Marketing</span></li>
-						<li><span class="badge text-bg-light">Start-up</span></li>
-						<li><span class="badge text-bg-light">Business planning</span></li>
-						<li><span class="badge text-bg-light">+2</span></li>
-					</ul>
-				</div>
-				<div class="card-filter-area mb-5">
-					<div class="title"><strong>Industry speciality</strong></div>
-					<ul class="card-badges list-unstyled list-inline">
-						<li><span class="badge text-bg-light">Mining</span></li>
-						<li><span class="badge text-bg-light">Retail trade</span></li>
-						<li><span class="badge text-bg-light">Construction</span></li>
-						<li><span class="badge text-bg-light">+2</span></li>
-					</ul>
-				</div>
-				<div class="card-filter-area">
-					<div class="title"><strong>Languages serviced</strong></div>
-					<ul class="card-badges list-unstyled list-inline">
-						<li>English, Vietnamese</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-		<div class="bottom-note">
-			<h2>Disclaimer</h2>
-			The list provided is intended for use as a general reference only, and does not constitute an endorsement of the
-			businesses, their product or service. Applicants should undertake their own due diligence on the service provider
-			they engage.
+    <div class="page-content__wrap">
+        <div class="top-content">
+            <div clas="top-content-title">
+                <Strong>About</Strong>
+            </div>
+            <div class="top-content-logo-hold">
+                <img src="assets/images/business-logo-1.png" alt="Placeholder" />
+            </div>
+            <div class="top-content-text-wrap">
+                <p>
+                    We provide comprehensive business IT and Marketing solutions. Our strategy is backed by a proven record of
+                    enhancing
+                    the return on investment for your business.We provide comprehensive business IT and Marketing solutions. Our
+                    strategy is backed by a proven record of enhancing the return on investment for your business. We provide
+                    comprehensive business IT and Marketing solutions. Our strategy is backed by a proven record of enhancing the
+                    return
+                    on investment for your business. We provide comprehensive business IT and Marketing solutions.</p>
+                <p>
+                    Our strategy is backed by a proven record of enhancing the return on investment for your business. We provide
+                    comprehensive business IT and Marketing solutions. Our strategy is backed by a proven record of enhancing the
+                    return
+                    on investment for your business. We provide comprehensive business IT and Marketing solutions. Our strategy is
+                    backed by a proven record of enhancing the return on investment for your business. We provide comprehensive
+                    business
+                    IT and Marketing solutions. Our strategy is backed by a proven record of enhancing the return on investment
+                    for
+                    your
+                    business.
+                </p>
+                <p><a href="#" class="read-more link-bold"> Read more</a></p>
+                <div class="meta-data mb-5">
+                    <div class="meta-data-row meta-data-row--address mb-2">
+                        <div class="meta-data__label"><strong>Address</strong></div>
+                        <div class="meta-data__value">Unit 505 / 66 Smith Street, Darwin City, Northern Territory, Australia, 0800
+                        </div>
+                    </div>
+                    <div class="meta-data-row meta-data-row--phone">
+                        <div class="meta-data__label"><strong>Phone</strong></div>
+                        <div class="meta-data__value"><a href="tel:0470695869">0470695869</a>
+                        </div>
+                    </div>
+                    <div class="meta-data-row meta-data-row--email">
+                        <div class="meta-data__label"><strong>Email</strong></div>
+                        <div class="meta-data__value"><a href="nailto:info@21webs.com.au">info@21webs.com.au</a>
+                        </div>
+                    </div>
+                    <div class="meta-data-row meta-data-row--web">
+                        <div class="meta-data__label"><strong>Website</strong></div>
+                        <div class="meta-data__value"><a href="https://21webs.com.au">21webs.com.au</a>
+                        </div>
+                    </div>
+                    <div class="meta-data-row meta-data-row--social">
+                        <div class="meta-data__label"><strong>Social</strong></div>
+                        <div class="meta-data__value">
+                            <a href="#"><i class="fa-brands fa-square-facebook"></i></a>
+                            <a href="#"><i class="fa-brands fa-square-instagram"></i></a>
+                            <a href="#"><i class="fa-brands fa-square-x-twitter"></i></a>
+                            <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-filter-area mb-5">
+                    <div class="title"><strong>Service area</strong></div>
+                    <ul class="card-badges list-unstyled list-inline">
+                        <li><span class="badge text-bg-light">Sales</span></li>
+                        <li><span class="badge text-bg-light">Marketing</span></li>
+                        <li><span class="badge text-bg-light">Start-up</span></li>
+                        <li><span class="badge text-bg-light">Business planning</span></li>
+                        <li><span class="badge text-bg-light">+2</span></li>
+                    </ul>
+                </div>
+                <div class="card-filter-area mb-5">
+                    <div class="title"><strong>Industry speciality</strong></div>
+                    <ul class="card-badges list-unstyled list-inline">
+                        <li><span class="badge text-bg-light">Mining</span></li>
+                        <li><span class="badge text-bg-light">Retail trade</span></li>
+                        <li><span class="badge text-bg-light">Construction</span></li>
+                        <li><span class="badge text-bg-light">+2</span></li>
+                    </ul>
+                </div>
+                <div class="card-filter-area">
+                    <div class="title"><strong>Languages serviced</strong></div>
+                    <ul class="card-badges list-unstyled list-inline">
+                        <li>English, Vietnamese</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="bottom-note">
+            <h2>Disclaimer</h2>
+            The list provided is intended for use as a general reference only, and does not constitute an endorsement of the
+            businesses, their product or service. Applicants should undertake their own due diligence on the service provider
+            they engage.
 
-			The directory of service providers is available on the understanding that while the Department aims to maintain
-			complete, accurate, and up-to-date information, it does not warrant the accuracy of the information provided and
-			will accept no responsibility for reliance upon it nor responsible for any errors or omissions on the list and
-			additional responsibilities of listed service providers. 
+            The directory of service providers is available on the understanding that while the Department aims to maintain
+            complete, accurate, and up-to-date information, it does not warrant the accuracy of the information provided and
+            will accept no responsibility for reliance upon it nor responsible for any errors or omissions on the list and
+            additional responsibilities of listed service providers. 
 
-			If you believe the information on this page is incorrect or misleading, please email business.strategy@nt.gov.au
-		</div>
-	</div>
+            If you believe the information on this page is incorrect or misleading, please email business.strategy@nt.gov.au
+        </div>
+    </div>
 </div>

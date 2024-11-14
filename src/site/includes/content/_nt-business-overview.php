73b8711167221cdf80a8fbbbf6763bf1365922e6
Jagruk <jagruk.paudel@gmail.com>
2024-11-14 10:45:57 +0545

    text and image / variations

---
+++ src/site/includes/content/_nt-business-overview.php
@@ -0,0 +1,57 @@
+<section class="bg-arafura-10 ntg-wysiwyg-container">
+    <div class="container">
+        <div class="row justify-content-center text-center py-5">
+            <div class="col-md-7">
+                <p class="lead">Business goals</p>
+                <h2>The Territory is an exciting place to do business where you can stand out and achieve your goals</h2>
+                <p>Statistics consistently show that the businesses landscape in the Territory is healthy and thriving. Compared to the national average businesses are starting, growing and … at a higher rate</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="ntg-text-and-image-block py-7">
+    <div class="container">
+        <div class="row align-items-center gy-4">
+            <div class="col ntg-text-and-image-block__content">
+                <p class="lead">Industry sectors</p>
+                <h2>Industries keeping the NT successful</h2>
+                <p>The contribution of industry sectors to the NT economy is unique. There are 16 main industries that contribute to the business landscape and inform the support systems available to businesses and opportunities to individuals and investors. there are opportunities for you to capitalise on established industries or be supported to innovate into emerging sectors.</p>
+                <a href="#" class="btn btn-outline-primary btn-external rounded-pill">Button</a>
+                <a href="#" class="btn btn-outline-primary btn-external rounded-pill">Button 2</a>
+                <a href="#" class="btn btn-outline-primary btn-external rounded-pill">Button 3</a>
+                    <a href="#" class="btn btn-outline-primary btn-external rounded-pill">Button 4</a>
+                    <a href="#" class="btn btn-outline-primary btn-external rounded-pill">Button 5</a>
+            </div>
+            <div class="col-md-5 ntg-text-and-image-block__image">
+                <div class="ntg-text-and-image-block__image-wrapper">
+                    <img class="w-100" src="../assets/images/placeholder-banner.png" alt="Placeholder banner" />
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="ntg-text-and-image-block py-7 reverse vertical-btn-group bg-arafura-10">
+    <div class="container">
+        <div class="row align-items-center gy-4">
+            <div class="col ntg-text-and-image-block__content">
+                <p class="lead">Industry sectors</p>
+                <h2>Industries keeping the NT successful</h2>
+                <p>The contribution of industry sectors to the NT economy is unique. There are 16 main industries that contribute to the business landscape and inform the support systems available to businesses and opportunities to individuals and investors. there are opportunities for you to capitalise on established industries or be supported to innovate into emerging sectors.</p>
+
+                <a href="#" class="btn btn-outline-primary btn-external rounded-pill">Territory Business Centre</a>
+                <a href="#" class="btn btn-outline-primary btn-external rounded-pill">Small business champions</a>
+                <a href="#" class="btn btn-outline-primary btn-external rounded-pill">Small business champions 1</a>
+                <a href="#" class="btn btn-outline-primary btn-external rounded-pill">Small business champions 2</a>
+                <a href="#" class="btn btn-outline-primary btn-external rounded-pill">Small business champions 3</a>
+                <a href="#" class="btn btn-outline-primary btn-external rounded-pill">Small business champions 4</a>
+            </div>
+            <div class="col-md-5 ntg-text-and-image-block__image">
+                <div class="ntg-text-and-image-block__image-wrapper">
+                    <img class="w-100" src="../assets/images/placeholder-banner.png" alt="Placeholder banner" />
+                </div>
+            </div>
+        </div>
+    </div>
+</section>

5a37efb4ae0d50a18031adbebed2dd0732552ad9
Alex <alex@brainiumlabs.com.au>
2025-01-14 13:10:00 +0930

    search listing, pagination

---
+++ src/site/pages/search.php
@@ -0,0 +1,10 @@
+<?php
+ob_start();
+include('includes/content/_search.php');
+$content = ob_get_clean();
+
+ob_start();
+include('includes/layout/_search.php');
+$full_html = ob_get_clean();
+
+echo $full_html;

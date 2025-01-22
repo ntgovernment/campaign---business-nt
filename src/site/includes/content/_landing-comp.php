ee1c067e94fbd609333f72e9642f1761fe55b590
Alex <alex@brainiumlabs.com.au>
2025-01-22 10:23:42 +0930

    horizontal cards contain images

--- src/site/includes/content/_landing-comp.php
+++ src/site/includes/content/_landing-comp.php
@@ -6,9 +6,15 @@
                 <div class="card-body col-md-5">
                     <div class="card-icon">
                         <svg width="46" height="46" viewBox="0 0 46 46" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M43.9091 25.0909C43.2818 25.0909 42.8637 25.5092 42.8637 26.1364V39.7273C42.8637 40.3546 42.4454 40.7727 41.8182 40.7727H4.1818C3.55451 40.7727 3.13638 40.3545 3.13638 39.7273V26.1364C3.13638 25.5091 2.71815 25.0909 2.09095 25.0909C1.46375 25.0909 1.04553 25.5092 1.04553 26.1364V39.7273C1.04553 41.5046 2.4046 42.8637 4.18189 42.8637H41.8182C43.5955 42.8637 44.9546 41.5046 44.9546 39.7273V26.1364C44.9546 25.5091 44.5364 25.0909 43.9091 25.0909Z" fill="#1F1F5F"/>
-                            <path d="M42.8636 10.4546H3.13636C1.35907 10.4546 0 11.8137 0 13.5909V20.2819C0 21.7454 1.04542 23 2.40458 23.3137L18.8182 26.9727V30.3181C18.8182 30.9454 19.2364 31.3636 19.8636 31.3636H26.1364C26.7636 31.3636 27.1818 30.9453 27.1818 30.3181V26.9727L43.5954 23.3137C44.9546 23 46 21.7454 46 20.2818V13.5909C46 11.8137 44.6409 10.4546 42.8636 10.4546ZM25.0909 29.2727H20.9092V25.0909H25.0909V29.2727ZM43.9091 20.2818C43.9091 20.8045 43.5954 21.2227 43.0727 21.3272L27.1818 24.8818V24.0454C27.1818 23.4181 26.7636 23 26.1364 23H19.8636C19.2364 23 18.8182 23.4182 18.8182 24.0454V24.8818L2.92729 21.3273C2.40458 21.2227 2.09093 20.8046 2.09093 20.2819V13.5909C2.09093 12.9637 2.50916 12.5455 3.13636 12.5455H42.8636C43.4909 12.5455 43.9091 12.9637 43.9091 13.5909V20.2818Z" fill="#1F1F5F"/>
-                            <path d="M28.2273 3.13635H17.7727C15.9954 3.13635 14.6364 4.49542 14.6364 6.27271V7.31813C14.6364 7.94542 15.0546 8.36355 15.6818 8.36355C16.309 8.36355 16.7272 7.94533 16.7272 7.31813V6.27271C16.7272 5.64542 17.1454 5.22729 17.7726 5.22729H28.2272C28.8545 5.22729 29.2726 5.64551 29.2726 6.27271V7.31813C29.2726 7.94542 29.6908 8.36355 30.318 8.36355C30.9453 8.36355 31.3635 7.94533 31.3635 7.31813V6.27271C31.3636 4.49542 30.0046 3.13635 28.2273 3.13635Z" fill="#1F1F5F"/>
+                            <path
+                                d="M43.9091 25.0909C43.2818 25.0909 42.8637 25.5092 42.8637 26.1364V39.7273C42.8637 40.3546 42.4454 40.7727 41.8182 40.7727H4.1818C3.55451 40.7727 3.13638 40.3545 3.13638 39.7273V26.1364C3.13638 25.5091 2.71815 25.0909 2.09095 25.0909C1.46375 25.0909 1.04553 25.5092 1.04553 26.1364V39.7273C1.04553 41.5046 2.4046 42.8637 4.18189 42.8637H41.8182C43.5955 42.8637 44.9546 41.5046 44.9546 39.7273V26.1364C44.9546 25.5091 44.5364 25.0909 43.9091 25.0909Z"
+                                fill="#1F1F5F" />
+                            <path
+                                d="M42.8636 10.4546H3.13636C1.35907 10.4546 0 11.8137 0 13.5909V20.2819C0 21.7454 1.04542 23 2.40458 23.3137L18.8182 26.9727V30.3181C18.8182 30.9454 19.2364 31.3636 19.8636 31.3636H26.1364C26.7636 31.3636 27.1818 30.9453 27.1818 30.3181V26.9727L43.5954 23.3137C44.9546 23 46 21.7454 46 20.2818V13.5909C46 11.8137 44.6409 10.4546 42.8636 10.4546ZM25.0909 29.2727H20.9092V25.0909H25.0909V29.2727ZM43.9091 20.2818C43.9091 20.8045 43.5954 21.2227 43.0727 21.3272L27.1818 24.8818V24.0454C27.1818 23.4181 26.7636 23 26.1364 23H19.8636C19.2364 23 18.8182 23.4182 18.8182 24.0454V24.8818L2.92729 21.3273C2.40458 21.2227 2.09093 20.8046 2.09093 20.2819V13.5909C2.09093 12.9637 2.50916 12.5455 3.13636 12.5455H42.8636C43.4909 12.5455 43.9091 12.9637 43.9091 13.5909V20.2818Z"
+                                fill="#1F1F5F" />
+                            <path
+                                d="M28.2273 3.13635H17.7727C15.9954 3.13635 14.6364 4.49542 14.6364 6.27271V7.31813C14.6364 7.94542 15.0546 8.36355 15.6818 8.36355C16.309 8.36355 16.7272 7.94533 16.7272 7.31813V6.27271C16.7272 5.64542 17.1454 5.22729 17.7726 5.22729H28.2272C28.8545 5.22729 29.2726 5.64551 29.2726 6.27271V7.31813C29.2726 7.94542 29.6908 8.36355 30.318 8.36355C30.9453 8.36355 31.3635 7.94533 31.3635 7.31813V6.27271C31.3636 4.49542 30.0046 3.13635 28.2273 3.13635Z"
+                                fill="#1F1F5F" />
                         </svg>
                     </div>
                     <div class="card-content">
@@ -26,9 +32,15 @@
                 <div class="card-body col-md-5">
                     <div class="card-icon">
                         <svg width="46" height="46" viewBox="0 0 46 46" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M43.9091 25.0909C43.2818 25.0909 42.8637 25.5092 42.8637 26.1364V39.7273C42.8637 40.3546 42.4454 40.7727 41.8182 40.7727H4.1818C3.55451 40.7727 3.13638 40.3545 3.13638 39.7273V26.1364C3.13638 25.5091 2.71815 25.0909 2.09095 25.0909C1.46375 25.0909 1.04553 25.5092 1.04553 26.1364V39.7273C1.04553 41.5046 2.4046 42.8637 4.18189 42.8637H41.8182C43.5955 42.8637 44.9546 41.5046 44.9546 39.7273V26.1364C44.9546 25.5091 44.5364 25.0909 43.9091 25.0909Z" fill="#1F1F5F"/>
-                            <path d="M42.8636 10.4546H3.13636C1.35907 10.4546 0 11.8137 0 13.5909V20.2819C0 21.7454 1.04542 23 2.40458 23.3137L18.8182 26.9727V30.3181C18.8182 30.9454 19.2364 31.3636 19.8636 31.3636H26.1364C26.7636 31.3636 27.1818 30.9453 27.1818 30.3181V26.9727L43.5954 23.3137C44.9546 23 46 21.7454 46 20.2818V13.5909C46 11.8137 44.6409 10.4546 42.8636 10.4546ZM25.0909 29.2727H20.9092V25.0909H25.0909V29.2727ZM43.9091 20.2818C43.9091 20.8045 43.5954 21.2227 43.0727 21.3272L27.1818 24.8818V24.0454C27.1818 23.4181 26.7636 23 26.1364 23H19.8636C19.2364 23 18.8182 23.4182 18.8182 24.0454V24.8818L2.92729 21.3273C2.40458 21.2227 2.09093 20.8046 2.09093 20.2819V13.5909C2.09093 12.9637 2.50916 12.5455 3.13636 12.5455H42.8636C43.4909 12.5455 43.9091 12.9637 43.9091 13.5909V20.2818Z" fill="#1F1F5F"/>
-                            <path d="M28.2273 3.13635H17.7727C15.9954 3.13635 14.6364 4.49542 14.6364 6.27271V7.31813C14.6364 7.94542 15.0546 8.36355 15.6818 8.36355C16.309 8.36355 16.7272 7.94533 16.7272 7.31813V6.27271C16.7272 5.64542 17.1454 5.22729 17.7726 5.22729H28.2272C28.8545 5.22729 29.2726 5.64551 29.2726 6.27271V7.31813C29.2726 7.94542 29.6908 8.36355 30.318 8.36355C30.9453 8.36355 31.3635 7.94533 31.3635 7.31813V6.27271C31.3636 4.49542 30.0046 3.13635 28.2273 3.13635Z" fill="#1F1F5F"/>
+                            <path
+                                d="M43.9091 25.0909C43.2818 25.0909 42.8637 25.5092 42.8637 26.1364V39.7273C42.8637 40.3546 42.4454 40.7727 41.8182 40.7727H4.1818C3.55451 40.7727 3.13638 40.3545 3.13638 39.7273V26.1364C3.13638 25.5091 2.71815 25.0909 2.09095 25.0909C1.46375 25.0909 1.04553 25.5092 1.04553 26.1364V39.7273C1.04553 41.5046 2.4046 42.8637 4.18189 42.8637H41.8182C43.5955 42.8637 44.9546 41.5046 44.9546 39.7273V26.1364C44.9546 25.5091 44.5364 25.0909 43.9091 25.0909Z"
+                                fill="#1F1F5F" />
+                            <path
+                                d="M42.8636 10.4546H3.13636C1.35907 10.4546 0 11.8137 0 13.5909V20.2819C0 21.7454 1.04542 23 2.40458 23.3137L18.8182 26.9727V30.3181C18.8182 30.9454 19.2364 31.3636 19.8636 31.3636H26.1364C26.7636 31.3636 27.1818 30.9453 27.1818 30.3181V26.9727L43.5954 23.3137C44.9546 23 46 21.7454 46 20.2818V13.5909C46 11.8137 44.6409 10.4546 42.8636 10.4546ZM25.0909 29.2727H20.9092V25.0909H25.0909V29.2727ZM43.9091 20.2818C43.9091 20.8045 43.5954 21.2227 43.0727 21.3272L27.1818 24.8818V24.0454C27.1818 23.4181 26.7636 23 26.1364 23H19.8636C19.2364 23 18.8182 23.4182 18.8182 24.0454V24.8818L2.92729 21.3273C2.40458 21.2227 2.09093 20.8046 2.09093 20.2819V13.5909C2.09093 12.9637 2.50916 12.5455 3.13636 12.5455H42.8636C43.4909 12.5455 43.9091 12.9637 43.9091 13.5909V20.2818Z"
+                                fill="#1F1F5F" />
+                            <path
+                                d="M28.2273 3.13635H17.7727C15.9954 3.13635 14.6364 4.49542 14.6364 6.27271V7.31813C14.6364 7.94542 15.0546 8.36355 15.6818 8.36355C16.309 8.36355 16.7272 7.94533 16.7272 7.31813V6.27271C16.7272 5.64542 17.1454 5.22729 17.7726 5.22729H28.2272C28.8545 5.22729 29.2726 5.64551 29.2726 6.27271V7.31813C29.2726 7.94542 29.6908 8.36355 30.318 8.36355C30.9453 8.36355 31.3635 7.94533 31.3635 7.31813V6.27271C31.3636 4.49542 30.0046 3.13635 28.2273 3.13635Z"
+                                fill="#1F1F5F" />
                         </svg>
                     </div>
                     <div class="card-content">
@@ -50,108 +62,108 @@
         <div class="col-md-6">
             <div class="card card--image-card card--link-card">
                 <div class="row g-0">
-                  <div class="col-sm-4 image-overlay">
-                    <img src="../assets/images/katherine-river.png" class="img-fluid" alt="Card image">
-                  </div>
-                  <div class="col-sm-8">
-                    <div class="card-body">
-                        <h5 class="card-title h4">Start a business in the Northern Territory</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <div class="col-sm-4 image-overlay">
+                        <img src="../assets/images/katherine-river.png" class="img-fluid" alt="Card image">
                     </div>
-                    <div class="card-footer">
-                        <a href="#" class="link-chevron-right card-link stretched-link">Find out more</a>
+                    <div class="col-sm-8">
+                        <div class="card-body">
+                            <h5 class="card-title h4">Start a business in the Northern Territory</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                        <div class="card-footer">
+                            <a href="#" class="link-chevron-right card-link stretched-link">Find out more</a>
+                        </div>
                     </div>
-                  </div>
                 </div>
             </div>
         </div>
         <div class="col-md-6">
             <div class="card card--image-card card--link-card">
                 <div class="row g-0">
-                  <div class="col-sm-4 image-overlay">
-                    <img src="../assets/images/katherine-river.png" class="img-fluid" alt="Card image">
-                  </div>
-                  <div class="col-sm-8">
-                    <div class="card-body">
-                        <h5 class="card-title h4">Start a business in the Northern Territory</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <div class="col-sm-4 image-overlay">
+                        <img src="../assets/images/katherine-river.png" class="img-fluid" alt="Card image">
                     </div>
-                    <div class="card-footer">
-                        <a href="#" class="link-chevron-right card-link stretched-link">Find out more</a>
+                    <div class="col-sm-8">
+                        <div class="card-body">
+                            <h5 class="card-title h4">Start a business in the Northern Territory</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                        <div class="card-footer">
+                            <a href="#" class="link-chevron-right card-link stretched-link">Find out more</a>
+                        </div>
                     </div>
-                  </div>
                 </div>
             </div>
         </div>
         <div class="col-md-6">
             <div class="card card--image-card card--link-card">
                 <div class="row g-0">
-                  <div class="col-sm-4 image-overlay">
-                    <img src="../assets/images/katherine-river.png" class="img-fluid" alt="Card image">
-                  </div>
-                  <div class="col-sm-8">
-                    <div class="card-body">
-                        <h5 class="card-title h4">Start a business in the Northern Territory</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <div class="col-sm-4 image-overlay">
+                        <img src="../assets/images/katherine-river.png" class="img-fluid" alt="Card image">
                     </div>
-                    <div class="card-footer">
-                        <a href="#" class="link-chevron-right card-link stretched-link">Find out more</a>
+                    <div class="col-sm-8">
+                        <div class="card-body">
+                            <h5 class="card-title h4">Start a business in the Northern Territory</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                        <div class="card-footer">
+                            <a href="#" class="link-chevron-right card-link stretched-link">Find out more</a>
+                        </div>
                     </div>
-                  </div>
                 </div>
             </div>
         </div>
         <div class="col-md-6">
             <div class="card card--image-card card--link-card">
                 <div class="row g-0">
-                  <div class="col-sm-4 image-overlay">
-                    <img src="../assets/images/katherine-river.png" class="img-fluid" alt="Card image">
-                  </div>
-                  <div class="col-sm-8">
-                    <div class="card-body">
-                        <h5 class="card-title h4">Start a business in the Northern Territory</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <div class="col-sm-4 image-overlay">
+                        <img src="../assets/images/katherine-river.png" class="img-fluid" alt="Card image">
                     </div>
-                    <div class="card-footer">
-                        <a href="#" class="link-chevron-right card-link stretched-link">Find out more</a>
+                    <div class="col-sm-8">
+                        <div class="card-body">
+                            <h5 class="card-title h4">Start a business in the Northern Territory</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                        <div class="card-footer">
+                            <a href="#" class="link-chevron-right card-link stretched-link">Find out more</a>
+                        </div>
                     </div>
-                  </div>
                 </div>
             </div>
         </div>
         <div class="col-md-6">
             <div class="card card--image-card card--link-card">
                 <div class="row g-0">
-                  <div class="col-sm-4 image-overlay">
-                    <img src="../assets/images/katherine-river.png" class="img-fluid" alt="Card image">
-                  </div>
-                  <div class="col-sm-8">
-                    <div class="card-body">
-                        <h5 class="card-title h4">Start a business in the Northern Territory</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <div class="col-sm-4 image-overlay">
+                        <img src="../assets/images/katherine-river.png" class="img-fluid" alt="Card image">
                     </div>
-                    <div class="card-footer">
-                        <a href="#" class="link-chevron-right card-link stretched-link">Find out more</a>
+                    <div class="col-sm-8">
+                        <div class="card-body">
+                            <h5 class="card-title h4">Start a business in the Northern Territory</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                        <div class="card-footer">
+                            <a href="#" class="link-chevron-right card-link stretched-link">Find out more</a>
+                        </div>
                     </div>
-                  </div>
                 </div>
             </div>
         </div>
         <div class="col-md-6">
             <div class="card card--image-card card--link-card">
                 <div class="row g-0">
-                  <div class="col-sm-4 image-overlay">
-                    <img src="../assets/images/katherine-river.png" class="img-fluid" alt="Card image">
-                  </div>
-                  <div class="col-sm-8">
-                    <div class="card-body">
-                        <h5 class="card-title h4">Start a business in the Northern Territory</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <div class="col-sm-4 image-overlay">
+                        <img src="../assets/images/katherine-river.png" class="img-fluid" alt="Card image">
                     </div>
-                    <div class="card-footer">
-                        <a href="#" class="link-chevron-right card-link stretched-link">Find out more</a>
+                    <div class="col-sm-8">
+                        <div class="card-body">
+                            <h5 class="card-title h4">Start a business in the Northern Territory</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                        <div class="card-footer">
+                            <a href="#" class="link-chevron-right card-link stretched-link">Find out more</a>
+                        </div>
                     </div>
-                  </div>
                 </div>
             </div>
         </div>
